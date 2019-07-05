c4fa7bffdca01db22a4dc1f598ae0e52f592aaed
phillip <windowsyyf@gmail.com>
2019-07-04 23:23:12 -0400

    test

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  originalUserSettings:UserSettings={
+    name:null,
+    emailOffers:null,
+    interfaceStyle:null,
+    subscriptionType:null,
+    notes:null
+
+  };
+
+  userSettings:UserSettings={...this.originalUserSettings};
   constructor() { }
 
   ngOnInit() {
